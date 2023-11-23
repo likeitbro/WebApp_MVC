316836c6c766a79480751538157ea2623e6b1ad3
Stephen <sokhrannykhs@gmail.com>
2023-11-23 21:14:43 +0500

    Added data to DB into Category table via model builder, created migration, updated database

--- WebAppProject/Data/ApplicationDBContext.cs
+++ WebAppProject/Data/ApplicationDBContext.cs
@@ -11,5 +11,15 @@ namespace WebAppProject.Data
         }
 
         public DbSet<Category> Categories { get; set; } // Here Categories is the table name
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Drama", DisplayOrder = 1 }, // A problem may occur while sorting
+                new Category { Id = 3, Name = "Thriller", DisplayOrder = 2 },
+                new Category { Id = 4, Name = "Sci-Fi", DisplayOrder = 3 }
+                );
+        }
     }
 }
--- WebAppProject/Migrations/20231123160954_AddCategoriesToCategoryTable.Designer.cs
+++ WebAppProject/Migrations/20231123160954_AddCategoriesToCategoryTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAppProject.Data;
 
@@ -10,9 +11,11 @@ using WebAppProject.Data;
 namespace WebAppProject.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20231123160954_AddCategoriesToCategoryTable")]
+    partial class AddCategoriesToCategoryTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
