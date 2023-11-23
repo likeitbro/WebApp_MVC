38f0374e7335663262fce791dc48fe00f3287719
Stephen <sokhrannykhs@gmail.com>
2023-11-23 21:23:38 +0500

    Importing data from DB table to a local List of categories

--- WebAppProject/Controllers/CategoryController.cs
+++ WebAppProject/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebAppProject.Data;
+using WebAppProject.Models;
 
 namespace WebAppProject.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext dbContext) 
+        { 
+            _dbContext = dbContext;
+        }
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _dbContext.Categories.ToList();
             return View();
         }
     }
